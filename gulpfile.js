509fd675b1d302f3e4d0ecb784d25152132a9330
Hamid Adelyar <hamidadelyar@gmail.com>
2018-02-04 00:24:07 +0000

    Created skeleton for projects. Uses gulp to compile sass and browsersync

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+const PATH_SASS_FILES = 'sass/*.sass';
+const PATH_HTML_FILES = '**/*.html';
+const PATH_JS_FILES = 'js/**/*.js';
+/*
+gulp.task('task-name', function () {
+  return gulp.src('source-files') // Get source files with gulp.src
+    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+})
+
+*/
+
+
+gulp.task('sass', function(){
+  return gulp.src(PATH_SASS_FILES)
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+    	stream: true
+    }))
+});
+
+/*
+// Gulp watch syntax
+
+gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
+
+*/
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+	gulp.watch(PATH_SASS_FILES, ['sass']);
+	// Reloads the browser whenever HTML or JS files change
+	gulp.watch(PATH_HTML_FILES, browserSync.reload); 
+	gulp.watch(PATH_JS_FILES, browserSync.reload); 
+});
+
+/*
+
+gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'], function (){
+  // ...
+})
+
+*/
+
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: '.'
+		}
+	})
+})
